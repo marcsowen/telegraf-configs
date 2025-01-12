4421b2c20e94062a97846349ae2731d366753fe5
Marc Sowen <marc@sowen.net>
2025-01-12 17:18:30 +0100

    Added dirigera environmental sensors

---
+++ scripts/fetch_dirigera.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import json
+import sys
+from datetime import datetime, timedelta
+
+import requests
+import urllib3
+import yaml
+
+urllib3.disable_warnings()
+
+dirigera_ip="192.168.178.63"
+config = yaml.safe_load(open("/etc/dirigera.yaml"))
+
+headers = {'Authorization': 'Bearer ' + config["access_token"]}
+
+response = json.loads(requests.get("https://" + dirigera_ip + ":8443/v1/devices/", headers=headers, verify=False)
+                      .content.decode('UTF-8'))
+
+formatted_data = []
+
+for device in response:
+    if device["deviceType"] == "environmentSensor":
+        attributes = device["attributes"]
+        formatted_entry = {
+            "timestamp": device["lastSeen"],
+            "id": attributes["serialNumber"],
+            "name": attributes["customName"],
+            "productCode": attributes["productCode"],
+            "firmwareVersion": attributes["firmwareVersion"],
+            "hardwareVersion": attributes["hardwareVersion"],
+            "currentPM25": attributes["currentPM25"],
+            "currentRH": attributes["currentRH"],
+            "currentTemperature": attributes["currentTemperature"],
+            "maxMeasuredPM25": attributes["maxMeasuredPM25"],
+            "minMeasuredPM25": attributes["minMeasuredPM25"],
+            "vocIndex": attributes["vocIndex"],
+        }
+
+        formatted_data.append(formatted_entry)
+
+json.dump(formatted_data, sys.stdout)
